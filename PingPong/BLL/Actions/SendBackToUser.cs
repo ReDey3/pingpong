e6ef4f3a3b0cc5ee273c01a3a4b1da9babbec578
danyardo2 <danyardo22@gmail.com>
2022-01-19 17:59:13 +0200

    Added new converter from string to bytes

--- PingPong/BLL/Actions/SendBackToUser.cs
+++ PingPong/BLL/Actions/SendBackToUser.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using Common.Converters.Abstractions;
 using Common.Converters;
 
 namespace BLL.Actions
 {
     public class SendBackToUser : Abstractions.IAction<string>
     {
+        private 
         public void RunAction(Socket handler, string input)
         {
             handler.Send(Encoding.ASCII.GetBytes(input));
---
+++ PingPong/Common/Converters/StringToBytes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Converters.Abstractions;
+
+namespace Common.Converters
+{
+    public class StringToBytes : IStringConverter<byte[]>
+    {
+        public byte[] Convert(string dataToConvert)
+        {
+            return Encoding.ASCII.GetBytes(dataToConvert);
+        }
+    }
+}
