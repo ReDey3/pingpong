59f0f780ce409c46baf18de692c21db9400ee0d3
danyardo2 <danyardo22@gmail.com>
2022-01-19 16:03:56 +0200

    Finished understanding how socket lib works

--- PingPong/PingPong/Program.cs
+++ PingPong/PingPong/Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
+
 namespace PingPong
 {
     class Program 
@@ -12,29 +13,52 @@ namespace PingPong
             IPHostEntry host = Dns.GetHostEntry("localhost");
             IPAddress ipAddress = host.AddressList[0];
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 1337);
-
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(100);
             try
             {
-                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                listener.Bind(localEndPoint);
-                listener.Listen(100);
-                Console.WriteLine("Waiting for a connection...");
-                Socket handler = listener.Accept();
-                Console.WriteLine("User logged");
-                string data = null;
-                byte[] bytes = null;
-                
                 while (true)
                 {
-                    bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                   
-                    Console.WriteLine("Text received : {0}", data);
-                    data = ""; 
+                    Console.WriteLine("Waiting for a connection...");
+                    Socket handler = listener.Accept();
+                    Console.WriteLine("User logged");
+                    userHandler(handler);
                 }
             }
             catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static async void userHandler(Socket handler)
+        {
+            Console.WriteLine("User logged");
+            string data = null;
+            byte[] bytes = null;
+            try
+            {
+                await Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                        Console.WriteLine("Text received : {0}", data);
+                        byte[] msg = Encoding.ASCII.GetBytes(data);
+                        handler.Send(msg);
+                        data = "";
+                    }
+                });
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("User logged out.");
+            }
+            catch (Exception e)
             {
                 throw e;
             }
