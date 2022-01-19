0ce5c75c0ac6860c34e6f715499281d6c4dea4c1
danyardo2 <danyardo22@gmail.com>
2022-01-19 16:34:58 +0200

    Finished basic stage of common lib

---
+++ PingPong/Common/Converters/Abstractions/IConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Converters.Abstractions
+{
+    public interface IBytesConverter<T>
+    {
+        public T Convert(byte[] bytes, int bytesReq);
+    }
+}
---
+++ PingPong/Common/Converters/BytesToStringConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Converters
+{
+    public class BytesToStringConverter : Abstractions.IBytesConverter<string>
+    {
+        public string Convert(byte[] bytes, int bytesReq)
+        {
+            return Encoding.ASCII.GetString(bytes, 0, bytesReq);
+        }
+    }
+}
